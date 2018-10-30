5b3ed30e6cd221fc27c6e18bc2c089cb8c8e5e34
Leslie Zhou <zzhou2@mail.sfsu.edu>
2018-10-30 15:20:08 -0700

    connect to mongodb

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,34 @@
 package main.java;
+
 import static spark.Spark.*;
+import java.io.*;
+import com.mongodb.*;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+import java.util.*;
+
 
 public class Main {
 
     public static void main(String[] args) {
-      // staticFiles.externalLocation("public");
-      // http://sparkjava.com/documentation
-      port(1234);
-      // calling get will make your app start listening for the GET path with the /hello endpoint
-      get("/hello", (req, res) -> "Hello World");
+        // staticFiles.externalLocation("public");
+        // http://sparkjava.com/documentation
+        port(1234);
+        // calling get will make your app start listening for the GET path with the /hello endpoint
+
+        //MongoDB
+        MongoClient mongoClient;
+        mongoClient = MongoClients.create();
+        MongoDatabase db = mongoClient.getDatabase("REST2");
+        MongoCollection<Document> usersCollection = db.getCollection("users");
+        MongoCollection<Document> authCollection = db.getCollection("auth");
+        
+        get("/hello", (req, res) -> "Hello World");
+
 
     }
 }
